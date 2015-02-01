c274875257c7ccaea3c8aa90f6fc717004978f10
alxcancado <alxcancado@gmail.com>
2015-02-01 17:12:12 +1300

    Swift - First . Labels . Text Input . Buttons . Changing Label Text . Responding a Tap . Printf debugging

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        labelName.text = "A-ha!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        println("Hello")
     }
 
     override func didReceiveMemoryWarning() {
